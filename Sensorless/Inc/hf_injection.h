5bdeced729740ad86d7ed4baf55b4d59d9d7f424
Xnjikol <61531165+Xnjikol@users.noreply.github.com>
2025-09-09 13:20:35 +0800

    初步转换成方波注入

--- Sensorless/Inc/hf_injection.h
+++ Sensorless/Inc/hf_injection.h
@@ -94,7 +94,7 @@ void Hfi_Set_Theta_Err(float ref);
 
 void Hfi_Set_Speed_Err(float ref);
 
-Clark_t Hfi_Get_FilteredCurrent(Clark_t current);
+Clark_t Hfi_Process_Current(Clark_t current);
 
 Park_t Hfi_Get_Inject_Voltage(void);
 
